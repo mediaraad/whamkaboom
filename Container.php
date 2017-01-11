1d546d6527a7a4d9268771af8a06fb077a45f75b
hub <sens@xs4all.nl>
2017-01-11 17:35:38 +0100

    PDO connectie met dB veranderd in Container.php

--- Container.php
+++ Container.php
@@ -21,9 +21,19 @@ class Container
             $this->pdo = new PDO (
                 $this->configuration['db_dsn'],
                 $this->configuration['db_user'],
-                $this->configuration['db_pass']
+                $this->configuration['db_pass'],
+                array(
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
+                )
             );
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //$this->pdo->exec("SET NAMES 'utf8';");
+
+            // Onderstaande regel kan niet, omdat de verbinding met de dB al gemaakt is
+            // $this->pdo->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAME 'utf8'");
+            // de juiste code is:
+            // $this->pdo = new PDO($this->dsn, $this->username, $this->password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
         }
 
         return $this->pdo;
