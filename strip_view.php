7371ea30f838748d167abbe1188007b45641df54
hub <sens@xs4all.nl>
2017-03-06 10:30:40 +0100

    popup 3 werkt, maar ook niet naar wens

--- strip_view.php
+++ strip_view.php
@@ -10,6 +10,7 @@ require __DIR__ . '/bootstrap.php';
     <link rel='stylesheet' href='style.css' type="text/css">
 
     <script type="text/javascript" src="js3/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="js3/jquery-ui-1.12.1/jquery-ui.min.js"></script>
     <script type="text/javascript">
         function getHeld( deVar ) {
             var back_error;
@@ -41,25 +42,74 @@ require __DIR__ . '/bootstrap.php';
         }
 
 
+        var mouseX,mouseY,windowWidth,windowHeight;
+        var  popupLeft,popupTop;
+        $(document).ready(function(){
 
-        $(function() {
-            var moveLeft = 30;
-            var moveDown = -20;
+            $(document).mousemove(function(e){
+                mouseX = e.pageX;
+                mouseY = e.pageY;
+                //To Get the relative position
+                if( this.offsetLeft !=undefined)
+                    mouseX = e.pageX - this.offsetLeft;
+                if( this.offsetTop != undefined)
+                    mouseY = e.pageY; - this.offsetTop;
 
-            $('a.trigger').hover(function(e) {
+                if(mouseX < 0)
+                    mouseX =0;
+                if(mouseY < 0)
+                    mouseY = 0;
+
+                windowWidth  = $(window).width()+$(window).scrollLeft();
+                windowHeight = $(window).height()+$(window).scrollTop();
+            });
+
+            $('html').click(function(){
                 $('div#popp').show();
-                //.css('top', e.pageY + moveDown)
-                //.css('left', e.pageX + moveLeft)
-                //.appendTo('body');
-            }, function() {
-                $('div#popp').hide();
-            });
+                var popupWidth  = $('div#popp').outerWidth();
+                var popupHeight =  $('div#popp').outerHeight();
 
-            $('a.trigger').mousemove(function(e) {
-                $("div#popp").css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft);
-            });
+                if(mouseX+popupWidth > windowWidth)
+                    popupLeft = mouseX-popupWidth;
+                else
+                    popupLeft = mouseX;
 
+                if(mouseY+popupHeight > windowHeight)
+                    popupTop = mouseY-popupHeight;
+                else
+                    popupTop = mouseY;
+
+                if( popupLeft < $(window).scrollLeft()){
+                    popupLeft = $(window).scrollLeft();
+                }
+
+                if( popupTop < $(window).scrollTop()){
+                    popupTop = $(window).scrollTop();
+                }
+
+                if(popupLeft < 0 || popupLeft == undefined)
+                    popupLeft = 0;
+                if(popupTop < 0 || popupTop == undefined)
+                    popupTop = 0;
+
+                $('div#popp').offset({top:popupTop,left:popupLeft});
+            });
         });
+/*
+
+        $(document).mouseup(function (e)
+        {
+            var container = $("div#popp");
+
+            if (!container.is(e.target) // if the target of the click isn't the container...
+                && container.has(e.target).length === 0) // ... nor a descendant of the container
+            {
+                container.hide();
+            }
+        });
+*/
+
+
 
     </script>
    <style type="text/css">
@@ -99,7 +149,7 @@ echo $held . "*<br>";
 //$stripboek = new Stripboek();
 echo "<table>";
 foreach ($stripboeken as $stripboek) {
-    echo "<tr><td><a href='#'  class='trigger' onclick='javascript:getHeld(\"".$stripboek->getHeld() . "\");'> ".$stripboek->getHeld() . "</a></td><td>" . $stripboek->getTitle() ."</td><td>". $stripboek->getDeel() ."</td><td>".$stripboekLader->findTekenaarInStringById($stripboek->getTekenaar()) ."</td><td>" . $stripboek->getJaaruitgave() ."</td><td><a href=strip_bewerk.php>bewerk</a></td><td>verwijder</td><td>copy</td></tr>";
+    echo "<tr><td><span class='trigger' onclick='javascript:getHeld(\"".$stripboek->getHeld() . "\");'> ".$stripboek->getHeld() . "</span></td><td>" . $stripboek->getTitle() ."</td><td>". $stripboek->getDeel() ."</td><td>".$stripboekLader->findTekenaarInStringById($stripboek->getTekenaar()) ."</td><td>" . $stripboek->getJaaruitgave() ."</td><td><a href=strip_bewerk.php>bewerk</a></td><td>verwijder</td><td>copy</td></tr>";
 }
 echo "</table>";
 //var_dump($stripboeken);
