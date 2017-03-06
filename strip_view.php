eaeca20080a15b2f7772792020e0336375e06212
hub <sens@xs4all.nl>
2017-03-06 14:02:51 +0100

    popup 4 (domTT) werkt, maar ook niet naar wens

--- strip_view.php
+++ strip_view.php
@@ -8,6 +8,13 @@ require __DIR__ . '/bootstrap.php';
     <meta charset="utf-8">
     <title>Show Strip database</title>
     <link rel='stylesheet' href='style.css' type="text/css">
+    <script type="text/javascript" language="javascript" src="domTT/domLib.js"></script>
+    <script type="text/javascript" language="javascript" src="domTT/domTT.js"></script>
+    <script type="text/javascript" language="javascript" src="domTT/domTT_drag.js"></script>
+    <script>
+        var domTT_styleClass = 'domTTOverlib';
+    </script>
+</head>
 
     <script type="text/javascript" src="js3/jquery-3.1.1.min.js"></script>
     <script type="text/javascript" src="js3/jquery-ui-1.12.1/jquery-ui.min.js"></script>
@@ -42,97 +49,8 @@ require __DIR__ . '/bootstrap.php';
         }
 
 
-        var mouseX,mouseY,windowWidth,windowHeight;
-        var  popupLeft,popupTop;
-        $(document).ready(function(){
-
-            $(document).mousemove(function(e){
-                mouseX = e.pageX;
-                mouseY = e.pageY;
-                //To Get the relative position
-                if( this.offsetLeft !=undefined)
-                    mouseX = e.pageX - this.offsetLeft;
-                if( this.offsetTop != undefined)
-                    mouseY = e.pageY; - this.offsetTop;
-
-                if(mouseX < 0)
-                    mouseX =0;
-                if(mouseY < 0)
-                    mouseY = 0;
-
-                windowWidth  = $(window).width()+$(window).scrollLeft();
-                windowHeight = $(window).height()+$(window).scrollTop();
-            });
-
-            $('html').click(function(){
-                $('div#popp').show();
-                var popupWidth  = $('div#popp').outerWidth();
-                var popupHeight =  $('div#popp').outerHeight();
-
-                if(mouseX+popupWidth > windowWidth)
-                    popupLeft = mouseX-popupWidth;
-                else
-                    popupLeft = mouseX;
-
-                if(mouseY+popupHeight > windowHeight)
-                    popupTop = mouseY-popupHeight;
-                else
-                    popupTop = mouseY;
-
-                if( popupLeft < $(window).scrollLeft()){
-                    popupLeft = $(window).scrollLeft();
-                }
-
-                if( popupTop < $(window).scrollTop()){
-                    popupTop = $(window).scrollTop();
-                }
-
-                if(popupLeft < 0 || popupLeft == undefined)
-                    popupLeft = 0;
-                if(popupTop < 0 || popupTop == undefined)
-                    popupTop = 0;
-
-                $('div#popp').offset({top:popupTop,left:popupLeft});
-            });
-        });
-/*
-
-        $(document).mouseup(function (e)
-        {
-            var container = $("div#popp");
-
-            if (!container.is(e.target) // if the target of the click isn't the container...
-                && container.has(e.target).length === 0) // ... nor a descendant of the container
-            {
-                container.hide();
-            }
-        });
-*/
-
-
-
     </script>
-   <style type="text/css">
-       #popp {
-           display: none;
-           position: absolute;
-           width: 500px;
-           padding: 10px;
-           background: #eeeeee;
-           color: #000000;
-           border: 1px solid #1a1a1a;
-           font-size: 90%;
-       }
-       #outputdiv {
-           background-color: #eeeeee; border:1px solid red;
-           width: 500px;
-           text-align: left;
-           padding: 3px;
-           margin-top: 20px;
 
-           z-index: 9002;
-       }
-   </style>
 </head>
 <body>
 <div id="container">
@@ -149,7 +67,7 @@ echo $held . "*<br>";
 //$stripboek = new Stripboek();
 echo "<table>";
 foreach ($stripboeken as $stripboek) {
-    echo "<tr><td><span class='trigger' onclick='javascript:getHeld(\"".$stripboek->getHeld() . "\");'> ".$stripboek->getHeld() . "</span></td><td>" . $stripboek->getTitle() ."</td><td>". $stripboek->getDeel() ."</td><td>".$stripboekLader->findTekenaarInStringById($stripboek->getTekenaar()) ."</td><td>" . $stripboek->getJaaruitgave() ."</td><td><a href=strip_bewerk.php>bewerk</a></td><td>verwijder</td><td>copy</td></tr>";
+    echo "<tr><td><a href='#' onmouseover=\"domTT_activate(this, event, 'content', 'This tip stays put when active');\" onclick=\"return makeFalse(domTT_activate(this, event, 'caption', 'Verschillende titels', 'content', 'hallo', 'type', 'sticky', 'closeLink', '[close]', 'draggable', true));\">" .$stripboek->getHeld() . "</a></td><td>" . $stripboek->getTitle() ."</td><td>". $stripboek->getDeel() ."</td><td>".$stripboekLader->findTekenaarInStringById($stripboek->getTekenaar()) ."</td><td>" . $stripboek->getJaaruitgave() ."</td><td><a href=strip_bewerk.php>bewerk</a></td><td>verwijder</td><td>copy</td></tr>";
 }
 echo "</table>";
 //var_dump($stripboeken);
@@ -159,10 +77,10 @@ echo "</table>";
     <a href="#" id="trigger">test</a>
     <div id="popp" >
 
-    <div id="outputdiv"">
+
     klik op link!
 
-    </div>
+
     </di>
 </div>
 </body>
