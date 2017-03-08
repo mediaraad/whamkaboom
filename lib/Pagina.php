557be3a9def1f03e6d8230d0d4849a6b45d5eee3
hub <sens@xs4all.nl>
2017-03-08 18:26:38 +0100

    verbeteren class Pagina

--- lib/Pagina.php
+++ lib/Pagina.php
@@ -4,11 +4,11 @@ class Pagina {
 
     private $titelPagina;
 
-    function __construct($titel) {
+    public function __construct($titel) {
         $this->titelPagina = $titel;
     }
 
-    function paginaMenu($item) {
+    public function paginaMenu($item) {
         echo "<div id=menu> <ul>";
         if ($item == 1) echo "<li><a id=select href=\"index.php\">Home</a></li>";
         else echo "<li><a href=\"index.php\">Home</a></li>";
@@ -20,11 +20,11 @@ class Pagina {
         else echo "<li><a href=\"tekenaar_zoeken.php\">Tekenaar</a></li>";
         if ($item == 4) echo "<li><a id=select href=\"uitgever_change.php\">Uitgeverij</a></li>";
         else echo "<li><a href=\"uitgever_change.php\">Uitgeverij</a></li>";
-        echo "<li><a href=\"#\" style='color: #ffffff;'><div style='display: inline;width: 100px;'>&nbsp;</div></a></li><li><a href=\"logout.php\">Logout</a></li>";
+        echo "<li><a href=\"#\" style='color: #ffffff;'><div style='display: inline; border: solid 1px green;'>&nbsp;&nbsp;&nbsp;&nbsp;</div></a></li><li><a href=\"logout.php\">Logout</a></li>";
         echo "</ul> </div>";
     }
 
-    function footer() {
+    public function footer() {
         echo "<div id=footer>I am footer</div>";
     }
 
